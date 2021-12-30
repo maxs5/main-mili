888677b1439aa5786e06346e2ef290542a5f151c
maxs5 <93215847+maxs5@users.noreply.github.com>
2021-12-30 16:43:02 +0300

    init

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main (String [] args ){
+        int a = 30;
+        int b = 10;
+        int sum = a + b;
+        System.out.println ( sum );
+
+    }
+}
